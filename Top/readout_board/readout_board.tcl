172080fb370305017e6ad21c8dc04af7c0ee0fb1
Andrew Peck <andrew.peck@cern.ch>
2020-10-29 18:51:07 -0400

    SDIO maximum frequency

--- Top/readout_board/readout_board.tcl
+++ Top/readout_board/readout_board.tcl
@@ -59,6 +59,12 @@ upgrade_bd_cells [get_bd_cells {*}]
 set_property -dict [list CONFIG.PCW_SDIO_PERIPHERAL_FREQMHZ {33.333333333333} CONFIG.PCW_QSPI_GRP_SINGLE_SS_ENABLE {1}] [get_bd_cells processing_system]
 set_property -dict [list CONFIG.PCW_QSPI_GRP_SINGLE_SS_ENABLE {1} CONFIG.PCW_MIO_40_SLEW {slow}] [get_bd_cells processing_system]
 
+# Set an upper limit on SDIO frequency
+set sdio_freq [get_property CONFIG.PCW_SDIO_PERIPHERAL_FREQMHZ  [get_bd_cells processing_system]]
+if {50 < $sdio_freq} {
+    Msg Error "SDIO frequency $sdio_freq set to higher than maximum allowed value!"
+}
+
 make_wrapper -files [get_files $PATH_REPO/bd/gaps_ps_interface/gaps_ps_interface.bd] -top
 add_files -norecurse $PATH_REPO/bd/gaps_ps_interface/hdl/gaps_ps_interface_wrapper.vhd
 
