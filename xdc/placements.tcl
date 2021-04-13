d34fadbe8e939a4e947969a72c45f12c9161edaf
Andrew Peck <andrew.peck@cern.ch>
2021-04-12 23:50:52 -0400

    Fix Typo..

--- xdc/placements.tcl
+++ xdc/placements.tcl
@@ -10,4 +10,4 @@ place_cell  [get_cells trigger_mux_inst/drs_dwrite_o*]  SLICE_X43Y45/D6LUT
 place_cell  [get_cells -of_objects [get_nets trigger_mux_inst/ext_trigger_i]] \
              SLICE_X43Y45/C6LUT
 
-place_cell  [get_cells trigger_mux_inst/trigger_r_]      SLICE_X43Y45/C5FF
+place_cell  [get_cells trigger_mux_inst/trigger_r_*]      SLICE_X43Y45/C5FF
