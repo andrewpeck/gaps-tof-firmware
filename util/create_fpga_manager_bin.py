0e2a046178a1a3d034ea5d3fa63d80af8605f7f6
squinn2 <hubbleslaw@gmail.com>
2021-04-30 10:27:20 -0700

    Finish script

--- util/create_fpga_manager_bin.py
+++ util/create_fpga_manager_bin.py
@@ -1,3 +1,10 @@
+"""
+Author: Sean Quinn (spq@ucla.edu)
+client.py (c) UCLA 2021
+Desc: Uses Vivado bootgen to create bin from bit
+Created:  2021-04-30T17:24:11.188Z
+Modified: 2021-04-30T17:27:30.553Z
+"""
 import os
 import sys
 import subprocess
@@ -18,3 +25,27 @@ if bit_dir_exist == False:
 
 bin_dir_list = os.listdir(bit_dir)
 #print(bin_dir_lists)
+
+for i, folder in enumerate(bin_dir_list):
+  # Get file list of each version controlled folder
+  vcs_path = bit_dir + folder + '/'
+  vcs_dir_list = os.listdir(vcs_path)
+  # Filter bit files
+  bit_files = [x for x in vcs_dir_list if '.bit' in x and '.bin' not in x]
+  # Generate bin files
+  for j, bit_file in enumerate(bit_files):
+    # First create temp bif file
+    bif_text = "all:\n{\n\n  %s\n\n}" %(vcs_path + bit_file)
+    bif_fname = bit_file.replace(".bit",".bif")
+    with open(vcs_path + bif_fname, "w") as fd:
+      fd.write(bif_text)
+    cmd = ["bootgen",
+           "-image",
+           vcs_path + bif_fname,
+           "-arch",
+           "zynq",
+           "-w",
+           "-process_bitstream",
+           "bin"
+          ]
+    subprocess.run(cmd)
