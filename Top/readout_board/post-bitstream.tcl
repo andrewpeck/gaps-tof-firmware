1f424160758a0c5282ecb2000204dba7f31f5078
squinn2 <hubbleslaw@gmail.com>
2021-06-17 13:37:25 -0700

    Disable xsa gen. Otherwise results in error with 2020.1

--- Top/readout_board/post-bitstream.tcl
+++ Top/readout_board/post-bitstream.tcl
@@ -1,2 +1,4 @@
-set dst_xsa [file normalize "$dst_dir/${proj_name}\-$describe.xsa"]
-write_hw_platform -fixed -force -include_bit -file "$dst_xsa"
+#NOTE: these commands fail with 2020.1 for some reason
+#TODO: find magic commands to get working with 2020.1
+#set dst_xsa [file normalize "$dst_dir/${proj_name}\-$describe.xsa"]
+#write_hw_platform -fixed -force -include_bit -file "$dst_xsa"
