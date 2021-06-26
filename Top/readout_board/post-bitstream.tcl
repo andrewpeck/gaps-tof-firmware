309675a5ab244d61d1f3a94c22043b34b230a7af
Andrew Peck <andrew.peck@cern.ch>
2021-06-26 11:57:44 -0400

    Comment out (currently unused) ILA for 2020.1

--- Top/readout_board/post-bitstream.tcl
+++ Top/readout_board/post-bitstream.tcl
@@ -1,2 +1,3 @@
+# If using 2021.1 c.f. https://www.xilinx.com/support/answers/75210.html
 set dst_xsa [file normalize "$dst_dir/${proj_name}\-$describe.xsa"]
 write_hw_platform -fixed -force -include_bit -file "$dst_xsa"
