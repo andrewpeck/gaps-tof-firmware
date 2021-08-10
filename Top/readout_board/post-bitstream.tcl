03ee55a296b06b0e34c22c2928ff976c86943e50
Andrew Peck <andrew.peck@cern.ch>
2021-08-09 21:12:32 -0400

    Generate error on timing error

--- Top/readout_board/post-bitstream.tcl
+++ Top/readout_board/post-bitstream.tcl
@@ -1,3 +1,8 @@
 # If using 2021.1 c.f. https://www.xilinx.com/support/answers/75210.html
 set dst_xsa [file normalize "$dst_dir/${proj_name}\-$describe.xsa"]
 write_hw_platform -fixed -force -include_bit -file "$dst_xsa"
+
+# Generate error on timing error
+if [expr {[get_property SLACK [get_timing_paths -delay_type min_max]] < 0}] {
+    error "ERROR: Timing failed"
+}
