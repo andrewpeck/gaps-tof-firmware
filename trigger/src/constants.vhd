1b6863ae0795c827c78d3740e5ec29e607f28f4b
Andrew Peck <andrew.peck@cern.ch>
2022-05-09 20:49:07 -0400

    trg: fix constant w/ latest ccb

--- trigger/src/constants.vhd
+++ trigger/src/constants.vhd
@@ -9,7 +9,7 @@ package constants is
   constant NUM_RB_OUTPUTS  : positive := NUM_RBS*1;  -- number of output links
 
   --
-  constant NUM_LTS         : positive := 20;                         -- number of lt boards in the system
+  constant NUM_LTS         : positive := NUM_DSI*5;                  -- number of lt boards in the system
   constant NUM_LT_CHANNELS : positive := 16;                         -- number of hit channels per lt
   constant NUM_LT_MT_LINKS : positive := 3;                          -- number of links per mt
   constant NUM_LT_INPUTS   : positive := NUM_LT_MT_LINKS * NUM_LTS;  -- number of links in the system
