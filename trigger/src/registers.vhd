d374a0a63fda66b4ff0793c867845dac9892370b
Andrew Peck <andrew.peck@cern.ch>
2022-10-05 09:41:29 -0400

    trg: update generated registers.vhd

--- trigger/src/registers.vhd
+++ trigger/src/registers.vhd
@@ -10,13 +10,37 @@ package registers is
     -- Implements various control and monitoring functions of the DRS Logic
     --============================================================================
 
-    constant REG_MT_NUM_REGS : integer := 130;
+    constant REG_MT_NUM_REGS : integer := 136;
     constant REG_MT_ADDRESS_MSB : integer := 9;
     constant REG_MT_ADDRESS_LSB : integer := 0;
     constant REG_MT_LOOPBACK_ADDR    : std_logic_vector(9 downto 0) := "00" & x"00";
     constant REG_MT_LOOPBACK_MSB    : integer := 31;
     constant REG_MT_LOOPBACK_LSB     : integer := 0;
     constant REG_MT_LOOPBACK_DEFAULT : std_logic_vector(31 downto 0) := x"00000000";
+
+    constant REG_MT_CLOCK_RATE_ADDR    : std_logic_vector(9 downto 0) := "00" & x"01";
+    constant REG_MT_CLOCK_RATE_MSB    : integer := 31;
+    constant REG_MT_CLOCK_RATE_LSB     : integer := 0;
+
+    constant REG_MT_FB_CLOCK_RATE_0_ADDR    : std_logic_vector(9 downto 0) := "00" & x"02";
+    constant REG_MT_FB_CLOCK_RATE_0_MSB    : integer := 31;
+    constant REG_MT_FB_CLOCK_RATE_0_LSB     : integer := 0;
+
+    constant REG_MT_FB_CLOCK_RATE_1_ADDR    : std_logic_vector(9 downto 0) := "00" & x"03";
+    constant REG_MT_FB_CLOCK_RATE_1_MSB    : integer := 31;
+    constant REG_MT_FB_CLOCK_RATE_1_LSB     : integer := 0;
+
+    constant REG_MT_FB_CLOCK_RATE_2_ADDR    : std_logic_vector(9 downto 0) := "00" & x"04";
+    constant REG_MT_FB_CLOCK_RATE_2_MSB    : integer := 31;
+    constant REG_MT_FB_CLOCK_RATE_2_LSB     : integer := 0;
+
+    constant REG_MT_FB_CLOCK_RATE_3_ADDR    : std_logic_vector(9 downto 0) := "00" & x"05";
+    constant REG_MT_FB_CLOCK_RATE_3_MSB    : integer := 31;
+    constant REG_MT_FB_CLOCK_RATE_3_LSB     : integer := 0;
+
+    constant REG_MT_FB_CLOCK_RATE_4_ADDR    : std_logic_vector(9 downto 0) := "00" & x"06";
+    constant REG_MT_FB_CLOCK_RATE_4_MSB    : integer := 31;
+    constant REG_MT_FB_CLOCK_RATE_4_LSB     : integer := 0;
 
     constant REG_MT_PULSE_STRETCH_ADDR    : std_logic_vector(9 downto 0) := "00" & x"0f";
     constant REG_MT_PULSE_STRETCH_MSB    : integer := 3;
