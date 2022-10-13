e6d6deec71f01908e118ab2b2c0e87524dfc0104
Andrew Peck <andrew.peck@cern.ch>
2022-10-13 16:25:29 -0400

    drs: add a programmable delay line to the dwrite signal

--- drs/src/trigger_mux.vhd
+++ drs/src/trigger_mux.vhd
@@ -3,6 +3,9 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_misc.all;
 use ieee.numeric_std.all;
 
+library unisim;
+use unisim.vcomponents.all;
+
 entity trigger_mux is
 
   generic(
@@ -19,6 +22,8 @@ entity trigger_mux is
 
     -- software trigger
     force_trig : in std_logic;
+
+    delay_i : in  std_logic_vector (11 downto 0);
 
     -- master trigger
     master_trigger : in std_logic;
@@ -42,16 +47,38 @@ architecture behavioral of trigger_mux is
   signal trigger_os     : std_logic                         := '0';
   signal trigger_os_cnt : natural range 0 to TRIGGER_OS_MAX := 0;
 
-  signal trigger_r     : std_logic := '0';
-  signal trigger_rr    : std_logic := '0';
-  signal trigger       : std_logic := '0';
-  signal trigger_r_neg : std_logic := '0';
+  signal trigger_r      : std_logic := '0';
+  signal trigger_rr     : std_logic := '0';
+  signal trigger        : std_logic := '0';
+  signal trigger_dly    : std_logic := '0';
+  signal trigger_r_neg  : std_logic := '0';
 
   -- put a dont touch to allow manual placement
   attribute DONT_TOUCH : string;
   attribute DONT_TOUCH of trigger, trigger_r : signal is "true";
 
+  signal trigger_delay_line           : std_logic_vector(4095 downto 0);
+  -- Tell P&R to not optimize away the trigger_delay_line array
+  attribute keep : string;
+  attribute keep of trigger_delay_line : signal is "true";
+
 begin
+
+  -- buffer chain delay for hardware trigger
+  trigger_delay_line(0) <= trigger;
+
+  trigger_dly <= trigger_delay_line(to_integer(unsigned(delay_i)));
+
+  delayed_trig_gen : for bit_no in 1 to 4095 generate
+    LUT1_inst : LUT1
+      generic map (
+        INIT => "10"
+        )
+      port map (
+        O  => trigger_delay_line(bit_no),
+        I0 => trigger_delay_line(bit_no-1)
+        );
+  end generate;
 
   process (clock) is
   begin
@@ -111,7 +138,7 @@ begin
   --
   trigger <= ext_trigger or master_trigger or force_trig_ff;
 
-  dwrite_o <= not (trigger or trigger_os or trigger_r);
+  dwrite_o <= not (trigger_dly or trigger_os or trigger_r);
 
   -- rising edge only
   trigger_o <= '1' when trigger_r = '1' and trigger_rr = '0' else '0';
@@ -120,7 +147,7 @@ begin
   begin
     if (rising_edge(clock)) then
 
-      trigger_r  <= trigger;
+      trigger_r  <= trigger_dly;
       trigger_rr <= trigger_r or trigger_r_neg;
 
       if (trigger_r = '1') then
@@ -141,7 +168,7 @@ begin
   process (clock) is
   begin
     if (falling_edge(clock)) then
-      trigger_r_neg <= trigger;
+      trigger_r_neg <= trigger_dly;
     end if;
   end process;
 
