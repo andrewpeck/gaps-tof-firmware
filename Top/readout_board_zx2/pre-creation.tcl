545fe605c0481df003d9d68d0eb271762f6dac11
Andrew Peck <andrew.peck@cern.ch>
2022-10-15 11:34:33 -0400

    zx2: use some really nasty git filter to get a clean build

---
+++ Top/readout_board_zx2/pre-creation.tcl
@@ -0,0 +1,10 @@
+set filter {sed 's/xc7z010/xc7z020/' | \
+                sed 's/clg400/clg484/' | \
+                sed 's/xspeedgrade">.*</xspeedgrade">-2</' | \
+                sed 's/SPEEDGRADE">.*</SPEEDGRADE">-2</'}
+exec git config filter.reset_xci.clean $filter
+exec git config filter.reset_xci.smudge $filter
+
+set filter {sed -E s/\(--\)*\(.*emio.*\)/\\2/}
+exec git config filter.emio_filter.smudge $filter
+exec git config filter.emio_filter.clean $filter
