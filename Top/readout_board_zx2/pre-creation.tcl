8136f2dac26816a35ea6a922713846704840f8ab
Andrew Peck <andrew.peck@cern.ch>
2022-10-15 13:30:13 -0400

    zx2: use explicit git config path

--- Top/readout_board_zx2/pre-creation.tcl
+++ Top/readout_board_zx2/pre-creation.tcl
@@ -1,10 +1,2 @@
-set filter {sed 's/xc7z010/xc7z020/' | \
-                sed 's/clg400/clg484/' | \
-                sed 's/xspeedgrade">.*</xspeedgrade">-2</' | \
-                sed 's/SPEEDGRADE">.*</SPEEDGRADE">-2</'}
-exec git config --local filter.reset_xci.clean $filter
-exec git config --local filter.reset_xci.smudge $filter
-
-set filter {sed -E s/\(--\)*\(.*emio.*\)/\\2/}
-exec git config --local filter.emio_filter.smudge $filter
-exec git config --local filter.emio_filter.clean $filter
+set script_path "[file normalize [file dirname [info script]]]"
+puts [exec sh $script_path/setup-gitfilters.sh]
