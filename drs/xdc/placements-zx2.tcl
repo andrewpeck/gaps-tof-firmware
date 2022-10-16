631db3e8bc273d27043353df15a7bafb776f5731
Andrew Peck <andrew.peck@cern.ch>
2022-10-16 14:45:09 -0400

    zx2: disable hand placements
    (for now)

--- drs/xdc/placements-zx2.tcl
+++ drs/xdc/placements-zx2.tcl
@@ -5,9 +5,9 @@
 # hand place the dwrite output luts somewhere close to the dwrite_obuf
 # seems to help keep the routing time down and more consistent
 # keep them in the same slice
-place_cell  [get_cells trigger_mux_inst/drs_dwrite_o*]  SLICE_X43Y45/D6LUT
+# place_cell  [get_cells trigger_mux_inst/drs_dwrite_o*]  SLICE_X43Y45/D6LUT
 
-place_cell  [get_cells -of_objects [get_nets trigger_mux_inst/ext_trigger_i]] \
-             SLICE_X43Y45/C6LUT
+# place_cell  [get_cells -of_objects [get_nets trigger_mux_inst/ext_trigger_i]] \
+#              SLICE_X43Y45/C6LUT
 
-place_cell  [get_cells trigger_mux_inst/trigger_r_*]      SLICE_X43Y45/C5FF
+# place_cell  [get_cells trigger_mux_inst/trigger_r_*]      SLICE_X43Y45/C5FF
