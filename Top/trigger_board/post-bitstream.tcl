a142915d8ba17452105c6071256edd9e00d3081a
Andrew Peck <andrew.peck@cern.ch>
2022-12-08 11:07:08 -0500

    mt: copy program scripts into bin dir

---
+++ Top/trigger_board/post-bitstream.tcl
@@ -0,0 +1,2 @@
+file copy -force [file normalize $dst_dir/../../Top/trigger_board/program.tcl] $dst_dir/program.tcl
+file copy -force [file normalize $dst_dir/../../Top/trigger_board/program.sh] $dst_dir/program.sh
