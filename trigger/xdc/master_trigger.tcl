52e41fbe4bf03e49a1062754d957a0d32bd97fc0
Andrew Peck <andrew.peck@cern.ch>
2022-12-08 21:35:13 -0500

    mt: set config mode in bitstream

--- trigger/xdc/master_trigger.tcl
+++ trigger/xdc/master_trigger.tcl
@@ -6,6 +6,8 @@
 
 set_property CFGBVS VCCO [current_design]
 set_property CONFIG_VOLTAGE 3.3 [current_design]
+set_property CONFIG_MODE SPIx4 [current_design]
+set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
 
 ################################################################################
 # Input Clocks
