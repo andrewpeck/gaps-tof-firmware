cc81f6f215790557e0c393d4b896ca079202e21e
Andrew Peck <andrew.peck@cern.ch>
2022-12-11 21:51:34 -0500

    mt: add a copy of pico_hal into the output zip

--- Top/trigger_board/post-bitstream.tcl
+++ Top/trigger_board/post-bitstream.tcl
@@ -1,2 +1,3 @@
 file copy -force [file normalize $dst_dir/../../Top/trigger_board/program.tcl] $dst_dir/program.tcl
 file copy -force [file normalize $dst_dir/../../Top/trigger_board/program.sh] $dst_dir/program.sh
+file copy -force [file normalize $dst_dir/../../util/pico_hal.py] $dst_dir/pico_hal.py
