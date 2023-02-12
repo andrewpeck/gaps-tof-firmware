5f27a50281662beaf6cf1478d590677308ec8f34
Andrew Peck <andrew.peck@cern.ch>
2023-02-12 11:47:33 -0500

    mtb: fix missing bracket in post-bitstream

--- Top/trigger_board/post-bitstream.tcl
+++ Top/trigger_board/post-bitstream.tcl
@@ -1,4 +1,4 @@
 file copy -force [file normalize $dst_dir/../../Top/trigger_board/program.tcl] $dst_dir/program.tcl
 file copy -force [file normalize $dst_dir/../../Top/trigger_board/program.sh] $dst_dir/program.sh
 file copy -force [file normalize $dst_dir/../../util/pico_hal.py] $dst_dir/pico_hal.py
-file copy -force [file normalize $dst_dir/../../util/mt_addresses.json $dst_dir/mt_addresses.json
+file copy -force [file normalize $dst_dir/../../util/mt_addresses.json] $dst_dir/mt_addresses.json
