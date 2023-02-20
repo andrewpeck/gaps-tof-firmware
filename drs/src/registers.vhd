bdb46f4b829039638a73edea1c3719531045321b
Andrew Peck <andrew.peck@cern.ch>
2023-02-20 10:53:34 -0500

    drs: update registers

--- drs/src/registers.vhd
+++ drs/src/registers.vhd
@@ -10,7 +10,7 @@ package registers is
     -- Implements various control and monitoring functions of the DRS Logic
     --============================================================================
 
-    constant REG_DRS_NUM_REGS : integer := 66;
+    constant REG_DRS_NUM_REGS : integer := 67;
     constant REG_DRS_ADDRESS_MSB : integer := 9;
     constant REG_DRS_ADDRESS_LSB : integer := 0;
     constant REG_CHIP_DMODE_ADDR    : std_logic_vector(9 downto 0) := "00" & x"00";
@@ -216,6 +216,10 @@ package registers is
     constant REG_TRIGGER_TRIGGER_ENABLE_ADDR    : std_logic_vector(9 downto 0) := "00" & x"47";
     constant REG_TRIGGER_TRIGGER_ENABLE_BIT    : integer := 0;
     constant REG_TRIGGER_TRIGGER_ENABLE_DEFAULT : std_logic := '0';
+
+    constant REG_TRIGGER_MT_EVENT_CNT_ADDR    : std_logic_vector(9 downto 0) := "00" & x"48";
+    constant REG_TRIGGER_MT_EVENT_CNT_MSB    : integer := 31;
+    constant REG_TRIGGER_MT_EVENT_CNT_LSB     : integer := 0;
 
     constant REG_COUNTERS_CNT_SEM_CORRECTION_ADDR    : std_logic_vector(9 downto 0) := "00" & x"50";
     constant REG_COUNTERS_CNT_SEM_CORRECTION_MSB    : integer := 15;
