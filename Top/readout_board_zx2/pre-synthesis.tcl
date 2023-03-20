6872cb8855eac734165bde70e9fee7f768628b8a
Andrew Peck <andrew.peck@cern.ch>
2023-03-20 19:47:42 -0400

    rb: improve sed filtering script
    it should not repeat comments now

--- Top/readout_board_zx2/pre-synthesis.tcl
+++ Top/readout_board_zx2/pre-synthesis.tcl
@@ -1,4 +1,4 @@
 source "[file normalize [file dirname [info script]]]/../pre-synthesis.tcl"
 
-exec sed -i "s/.*emio*./--&/" drs/src/ps_interface.vhd
-exec sed -i "s/.*emio*./--&/" drs/src/daq_board_top.vhd
+exec sed -i -e "s/--\\(emio.*\\)/--\\1/" drs/src/daq_board_top.vhd
+exec sed -i -e "s/--\\(emio.*\\)/--\\1/" drs/src/ps_interface.vhd
--- Top/readout_board_zx3/pre-synthesis.tcl
+++ Top/readout_board_zx3/pre-synthesis.tcl
@@ -1,4 +1,4 @@
 set PATH_REPO "[file normalize [file dirname [info script]]]/../pre-synthesis.tcl"
 
-exec sed -i  "s/.*\\(--\\)-*\\(.*emio.*\\)/  \\2/" drs/src/ps_interface.vhd
-exec sed -i  "s/.*\\(--\\)-*\\(.*emio.*\\)/  \\2/" drs/src/daq_board_top.vhd
+exec sed -i  "s/--\\(emio.*\\)/  \\1/" drs/src/ps_interface.vhd
+exec sed -i  "s/--\\(emio.*\\)/  \\1/" drs/src/daq_board_top.vhd
