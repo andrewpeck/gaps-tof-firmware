9a4d69e94a59851cb40b18a3a39c746522775688
Andrew Peck <peckandrew@gmail.com>
2024-12-13 16:16:08 -0800

    mtb: change default emulator wait time to zero

--- trigger/src/infra/registers.vhd
+++ trigger/src/infra/registers.vhd
@@ -92,7 +92,7 @@ package registers is
     constant REG_TIU_EMU_BUSY_CNT_ADDR    : std_logic_vector(9 downto 0) := "00" & x"0e";
     constant REG_TIU_EMU_BUSY_CNT_MSB    : integer := 31;
     constant REG_TIU_EMU_BUSY_CNT_LSB     : integer := 14;
-    constant REG_TIU_EMU_BUSY_CNT_DEFAULT : std_logic_vector(31 downto 14) := "00" & x"c350";
+    constant REG_TIU_EMU_BUSY_CNT_DEFAULT : std_logic_vector(31 downto 14) := "00" & x"0000";
 
     constant REG_TIU_BAD_ADDR    : std_logic_vector(9 downto 0) := "00" & x"0f";
     constant REG_TIU_BAD_BIT    : integer := 0;
